1f45b002698c12b7c8e087a0d67288762b8887f7
Sumitdulal <sumitdulal@hotmail.com>
2021-08-26 18:03:25 +0545

    latest update

---
+++ dashboard/scripts/script.js
@@ -0,0 +1,8 @@
+// document.getElementById("hamburger").onclick = function(){
+    
+//     document.getElementById("sidenav").classList.remove("col-md-1");
+//     document.getElementById("sidenav").classList.add("col-md-2");
+//     document.getElementById("dashpart").classList.remove("col-md-11");
+//     document.getElementById("dashpart").classList.add("col-md-10");
+//     document.getElementById("sidenav").style.display = "block";
+// }
